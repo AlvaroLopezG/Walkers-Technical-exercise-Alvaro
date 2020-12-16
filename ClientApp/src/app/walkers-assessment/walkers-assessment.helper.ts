027de7205c5542f6e123a45b6ecc6494c6bb93c3
Alvaro <alvarogelg@gmail.com>
2020-12-16 10:46:06 -0300

    Changed helper implementation so it passes the test

--- ClientApp/src/app/walkers-assessment/walkers-assessment.helper.ts
+++ ClientApp/src/app/walkers-assessment/walkers-assessment.helper.ts
@@ -5,7 +5,17 @@ interface IWalkersAssessmentHelper {
 export class WalkersAssessmentHelper implements IWalkersAssessmentHelper {
     public countTo(valueToCountTo: number) : string[] {
         var result = new Array<string>();
-        result.push("dummy");
+        for(let i = 1; i<= valueToCountTo; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                result.push("Walkers assessment");
+            } else if(i % 3 == 0) {
+                result.push("Walkers");
+            } else if(i % 5 == 0) {
+                result.push("assessment");
+            } else {
+                result.push(i.toString());
+            }
+        }
         return result;
     }
 }
