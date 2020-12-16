9d87f7f09282850c4ac4f585219d4f04e2cd591d
Alvaro <alvarogelg@gmail.com>
2020-12-16 10:28:47 -0300

    Added a form to the component

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WalkersAssessmentComponent } from './walkers-assessment/walkers-assessment.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { WalkersAssessmentComponent } from './walkers-assessment/walkers-assessm
     WalkersAssessmentComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' })
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ClientApp/src/app/walkers-assessment/walkers-assessment.component.ts
+++ ClientApp/src/app/walkers-assessment/walkers-assessment.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-walkers-assessment',
   templateUrl: './walkers-assessment.component.html',
   styleUrls: ['./walkers-assessment.component.css']
 })
-export class WalkersAssessmentComponent implements OnInit {
+export class WalkersAssessmentComponent {
+  walkersAssessmentForm;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    let numberControl = new FormControl(null);
+    this.walkersAssessmentForm = this.formBuilder.group({
+      number: numberControl
+    });
   }
 
+  onSubmit(data) {
+    console.log(data);
+  }
 }
